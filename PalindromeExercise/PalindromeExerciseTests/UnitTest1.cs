611ba0621720527be37b5f130f38955624556e3a
Kristina Hobbs <kjcagle86@gmail.com>
2022-08-23 14:05:33 -0400

    exercise completed

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -5,10 +5,17 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("applesauce", false)]
+        public void MyTest(string word, bool expected)
         {
-
+            //arrange
+            var tester = new WordSmith();
+            //act
+            bool actual = tester.IsAPalindrome(word);
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
